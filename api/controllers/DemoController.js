94892461f9ea0012d48f05310b9bab592e0bd052
CHRIS HALE <chrishale@CHRISs-MacBook.local>
2016-01-14 11:36:56 -0600

    Demo updates

--- api/controllers/DemoController.js
+++ api/controllers/DemoController.js
@@ -13,5 +13,19 @@ module.exports = {
             Users.watch(req.socket);
             console.log( 'User subscribed to ' + req.socket.id );
         }
+        else{
+            res.json(200, { "status":"connect via socket please"});
+        }
+    },
+    testFunction:function(req,res){
+        var params=req.params.all();
+        res.json(200, { token: jwToken.issue({id: user.id})});
+    },
+    findUsers:function(req,res){
+        var user_id="";
+        Users.find({"first_name":"Chris"}).exec(function(err, records){
+            res.json(200, records);
+        });
+
     }
 };
